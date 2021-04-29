2dac112db09752e2e62d7c1606e1a64289b7ae5f
CristianoSantan <cristiano.mer@gmail.com>
2021-04-29 10:08:25 -0300

    js popup

--- js/index.js
+++ js/index.js
@@ -18,12 +18,65 @@ navClose.addEventListener('click', () => {
     navContainer.style.width = "0";
 });
 // ----------------------------------------------------------- Pop Up
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup__close");
 
+if(popup) {
+    closePopup.addEventListener("click", () => {
+        popup.classList.add("hide__popup");
+    });
+
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+            popup.classList.remove("hide__popup");
+        }, 500);
+    });
+}
 // ----------------------------------------------------------- Fixed Navigation
+const navBar = document.querySelector(".navigation");
+const gotoTop = document.querySelector(".goto-top");
+const scrollLink = document.querySelectorAll(".scroll-Link");
 
-// ----------------------------------------------------------- Smooth Scroll
+// -------------------------------- Smooth Scroll
+Array.from(scrollLink).map(link => {
+    link.addEventListener("click", e => {
+        // prevent default
+        e.preventDefault();
 
-// ----------------------------------------------------------- Box NavBar
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        const navHeight = navBar.getBoundingClientRect().height;
+        const fixNav = navBar.classList.contains("fix__nav");
+        let position = element.offsetTop - navHeight;
 
+        if(!fixNav) {
+            position = position - navHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        }),
+        navContainer.style.left = "-30rem";
+        document.body.classList.remove("active");
+    })
+})
+// --------------------------------- Fix NavBar
+window.addEventListener("scroll", e => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navBar.getBoundingClientRect().height;
+
+    if(scrollHeight > navHeight) {
+        navBar.classList.add("fix__nav");
+    } else {
+        navBar.classList.remove("fix__nav");
+    }
+
+    if (scrollHeight > 300) {
+        gotoTop.classList.add("show-top");
+    } else {
+        gotoTop.classList.remove("show-top");
+    }
+})
 // ----------------------------------------------------------- AOS
 AOS.init();
